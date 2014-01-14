c1c151d41a4b6feacf97aff6a9a65848eab88bcb
Eric Ye <ericye16@gmail.com>
2014-01-14 09:32:21 -0500

    added jbox2d

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.World;
+
 public class GameEngine {
     private ArrayList<Entity> entities;
     private MapRenderer mapRenderer;
+    private World world;
+    private BodyDef bodyDef = new BodyDef();
 
     private void renderEntities() {
         for (Entity entity: entities) {
@@ -17,6 +24,22 @@ public class GameEngine {
     public GameEngine(MapRenderer mapRenderer) {
         assert(mapRenderer != null);
         this.mapRenderer = mapRenderer;
+
+        this.world = new World(new Vec2(0, 0));
+
+    }
+
+    private void addCollisionArea(float x, float y, float xsize, float ysize) {
+        bodyDef.type = BodyType.STATIC;
+        bodyDef.active = true;
+        bodyDef.allowSleep = true;
+        bodyDef.position = new Vec2(x, y);
+        world.createBody(bodyDef);
+        //todo: joints
+    }
+
+    public World getWorld() {
+        return world;
     }
 
 }
